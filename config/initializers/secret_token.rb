e0031f5fcb908926f0594b5909f9b01dd5e94f58
lanrion <dylan@beansmile.com>
2014-01-27 00:04:58 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeixinRailsMiddlewareExample::Application.config.secret_key_base = 'c481893f91cee271f58dc5614da9571e88206a5ae7b39db27ace7eeeadaad3d6a555d8c33fbaf46e37b28f55b0be70ccf73d7db23b3251ca3dbb44d4669dc54a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeixinRailsMiddlewareExample::Application.config.session_store :cookie_store, key: '_weixin_rails_middleware_example_session'
