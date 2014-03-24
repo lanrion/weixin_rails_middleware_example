d4c0942077c540586496a8469783314978831068
lanrion <huaitao-deng@foxmail.com>
2014-03-24 14:58:52 +0800

    clean codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 WeixinRailsMiddlewareExample::Application.routes.draw do
-  mount WeixinRailsMiddleware::Engine, at: WeixinRailsMiddleware.config.engine_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
